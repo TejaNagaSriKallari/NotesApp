83f335928dd53164e091fecd7217d15d41d631fe
TejaNagaSriKallari <143927488+TejaNagaSriKallari@users.noreply.github.com>
2025-02-05 17:22:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Make sure this line includes your JSX files
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
